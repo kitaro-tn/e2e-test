72afd6a9d49318e7f0b2c62218ba344cdf9d581b
tatsunori-nishikori <t.nishikori@cchan.tv>
2018-02-01 14:13:07 +0900

    add protractor sample

---
+++ spec/conf.js
@@ -0,0 +1,11 @@
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  capabilities: {
+    browserName: 'chrome'
+  },
+  specs: '**/*[sS]pec.js',
+  jasmineNodeOpts: {
+    showColors: true,
+  }
+};
