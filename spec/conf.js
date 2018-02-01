41f28c77d1ce395ac7075bed4d2d9be5f38723cf
tatsunori-nishikori <t.nishikori@cchan.tv>
2018-02-01 14:44:01 +0900

    modify chrome headless mode

--- spec/conf.js
+++ spec/conf.js
@@ -1,8 +1,11 @@
 exports.config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
   capabilities: {
-    browserName: 'chrome'
+    browserName: 'chrome',
+    chromeOptions: {
+      args: [ "--headless", "--disable-gpu", "--window-size=1280,1024" ]
+    }
   },
   specs: '**/*[sS]pec.js',
   jasmineNodeOpts: {
